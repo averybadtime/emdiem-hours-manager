9f400ff5531ed6bc4545277eb4f356c495ee40f2
JC <devjc23@gmail.com>
2019-12-02 14:53:21 -0500

    Permisos a Project manager de crear historias

--- src/pages/client/Stories.vue
+++ src/pages/client/Stories.vue
@@ -4,7 +4,7 @@
     <EditStoryModal @storyInfoUpdated="storyInfoUpdated" :story="selectedStory"/>
     <div class="d-flex justify-content-between">
       <h6 class="card-title mb-2">Historias</h6>
-      <button v-if="$isAdmin()"
+      <button v-if="$isAdmin() || $isPM()"
         class="btn btn-primary btn-icon-text mb-2"
         data-target="#NewStoryModal"
         data-toggle="modal">
